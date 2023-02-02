04193913a0ace0858636aac6d9abb89ba11598a0
jso4342 <jso4342@naver.com>
2023-02-03 04:52:25 +0900

    chore : 환경 설정

---
+++ src/main/java/com/shoplive/converter/ShopliveApplication.java
@@ -0,0 +1,13 @@
+package com.shoplive.converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopliveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopliveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shoplive/converter/ShopliveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoplive.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopliveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
