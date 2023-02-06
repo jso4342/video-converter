e27fcc8c111dc43eeefd7a25f64879707cab0442
jso4342 <jso4342@naver.com>
2023-02-06 14:51:09 +0900

    refactor : VideoNotFoundException 변경

--- src/main/java/com/shoplive/converter/application/video/service/VideoService.java
+++ src/main/java/com/shoplive/converter/application/video/service/VideoService.java
@@ -3,7 +3,8 @@ package com.shoplive.converter.application.video.service;
 import com.shoplive.converter.application.video.domain.Video;
 import com.shoplive.converter.application.video.dto.VideoDto.*;
 import com.shoplive.converter.application.video.repository.VideoRepository;
-import com.shoplive.converter.core.exception.ResourceNotFoundException;
+import com.shoplive.converter.core.exception.ErrorCode;
+import com.shoplive.converter.core.exception.customException.VideoNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,9 +18,9 @@ public class VideoService {
     }
     // 영상의 상세 정보를 조회할 수 있는 API
     @Transactional(readOnly = true)
-    public VideoResponse getVideoById(Long id) {
-        Video video = videoRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("해당 영상을 찾을 수 없습니다."));
+    public VideoResponse getVideoById(Long videoId) {
+        Video video = videoRepository.findById(videoId)
+                .orElseThrow(() -> new VideoNotFoundException("video id" + videoId, ErrorCode.VIDEO_NOT_FOUND));
 
         return VideoResponse.from(video);
     }
