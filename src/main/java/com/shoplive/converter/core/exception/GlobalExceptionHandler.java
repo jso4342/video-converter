273024cf6c49032e081ff02caeeaf0c4be6ee11f
jso4342 <jso4342@naver.com>
2023-02-06 14:50:27 +0900

    feat : ExceptionHandler 추가

---
+++ src/main/java/com/shoplive/converter/core/exception/GlobalExceptionHandler.java
@@ -0,0 +1,69 @@
+package com.shoplive.converter.core.exception;
+
+import com.shoplive.converter.core.exception.customException.VideoNotFoundException;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Iterator;
+
+import static com.shoplive.converter.core.exception.ErrorCode.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        ErrorResponse response = ErrorResponse.of(INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(response, response.httpStatus());
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ErrorResponse> handleException(
+            MissingServletRequestParameterException e) {
+        ErrorResponse response = ErrorResponse.of(INVALID_PARAMETER);
+        return new ResponseEntity<>(response, response.httpStatus());
+    }
+
+    @ExceptionHandler(VideoNotFoundException.class)
+    protected ResponseEntity<ErrorResponse> handleException(VideoNotFoundException e) {
+        ErrorResponse response = new ErrorResponse(e.getErrorCode().getStatus(),
+                e.getErrorCode().getMessage());
+        return ResponseEntity.status(response.httpStatus()).body(response);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    protected ResponseEntity<ErrorResponse> handleException(ConstraintViolationException e) {
+        String resultMessage = getResultMessage(e.getConstraintViolations().iterator());
+        ErrorResponse response = new ErrorResponse(HttpStatus.BAD_REQUEST, resultMessage);
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    protected String getResultMessage(final Iterator<ConstraintViolation<?>> violationIterator) {
+        final StringBuilder resultMessageBuilder = new StringBuilder();
+        while (violationIterator.hasNext()) {
+            final ConstraintViolation<?> constraintViolation = violationIterator.next();
+            resultMessageBuilder
+                    .append("['")
+                    .append(getPropertyName(constraintViolation.getPropertyPath().toString()))
+                    .append("' is '")
+                    .append(constraintViolation.getInvalidValue())
+                    .append("'. ")
+                    .append(constraintViolation.getMessage())
+                    .append("]");
+
+            if (violationIterator.hasNext()) {
+                resultMessageBuilder.append(", ");
+            }
+        }
+
+        return resultMessageBuilder.toString();
+    }
+
+    protected String getPropertyName(String propertyPath) {
+        return propertyPath.substring(propertyPath.lastIndexOf('.') + 1);
+    }
+}
