0f9c0231b5889125e25099e6f9ed0d58f51dd2e1
jso4342 <jso4342@naver.com>
2023-02-08 19:52:53 +0900

    style : 코딩 컨벤션 적용

--- src/main/java/com/shoplive/converter/application/video/dto/OriginalDto.java
+++ src/main/java/com/shoplive/converter/application/video/dto/OriginalDto.java
@@ -20,7 +20,8 @@ public class OriginalDto {
                     fileSize,
                     width,
                     height,
-                    videoUrl);
+                    videoUrl
+            );
         }
     }
 
--- src/main/java/com/shoplive/converter/application/video/dto/ResizedDto.java
+++ src/main/java/com/shoplive/converter/application/video/dto/ResizedDto.java
@@ -20,7 +20,8 @@ public class ResizedDto {
                     fileSize,
                     width,
                     height,
-                    videoUrl);
+                    videoUrl
+            );
         }
     }
 
