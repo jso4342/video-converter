fc4b3d1f37a023597bc435827a5094244f3c03bb
jso4342 <jso4342@naver.com>
2023-02-08 09:15:59 +0900

    feat : VideoDto에 VideoRequest record 추가

--- src/main/java/com/shoplive/converter/application/video/dto/VideoDto.java
+++ src/main/java/com/shoplive/converter/application/video/dto/VideoDto.java
@@ -3,11 +3,31 @@ package com.shoplive.converter.application.video.dto;
 import com.shoplive.converter.application.video.domain.Original;
 import com.shoplive.converter.application.video.domain.Resized;
 import com.shoplive.converter.application.video.domain.Video;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 
 public class VideoDto {
-//    public record StorageRequest
+    public record VideoRequest(
+            @NotBlank
+            String title,
+            @NotNull
+            Long originalId,
+            @NotNull
+            Long resizedId
+    ){
+        public Video toEntity(
+                Original original,
+                Resized resized
+        ) {
+            return new Video(
+                    title,
+                    original,
+                    resized
+            );
+        }
+    }
 
     public record VideoResponse(
             Long id,
