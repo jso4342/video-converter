a10e72bde998439e6dbaa4e4cdb303ae4a85ce46
jso4342 <jso4342@naver.com>
2023-02-08 09:32:46 +0900

    style : 코딩 컨벤션 적용

--- src/main/java/com/shoplive/converter/application/video/dto/VideoDto.java
+++ src/main/java/com/shoplive/converter/application/video/dto/VideoDto.java
@@ -35,7 +35,6 @@ public class VideoDto {
             Original original,
             Resized resized,
             LocalDateTime createdAt
-
     ) {
         public static VideoResponse from (Video video) {
             return new VideoResponse(
